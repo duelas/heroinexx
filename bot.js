c1c7b4e574758e4f584ef30f3fbcd6a89621bc1d
Hei Ho <heihei1994@hotmail.com>
2021-03-17 15:11:49 +0800

    4

--- bot.js
+++ bot.js
@@ -9,6 +9,11 @@ client.on('ready', readyDiscord);
 client.on('message' , messageHandler);
 
 
+const ascii_pardun = '⡏⣼⣿⣿⣿⢋⣾⣿⣿⣿⠏⠾⢿⡟⣼⣿⢸⣿⣿⣧⠹⣿⣿⠏⠶⠾⣦⢻⡇⣿ \n⢸⣿⣿⣿⡏⣾⣿⠟⣿⠁⣾⣿⠇⣴⢹⣿⡇⢹⣿⣿⡆⠏⣶⣿⣿⡿⢂⢸⡇⣿ \n⢸⣿⣿⡟⢸⡿⢫⡾⢇⣿⡶⣂⣶⣶⣾⣿⣿⡞⣿⣿⣿⠈⣶⡆⠶⣾⣿⢸⡇⣿ \n⣾⣿⣿⡇⠜⣤⠟⠄⠾⠛⠐⠶⢬⣹⣿⣿⣿⣷⢹⣿⣿⢸⣀⠶⠙⠛⠟⢄⡇⣿ \n⣿⣿⣿⠄⠊⡠⠠⠛⠁⠉⠉⡐⢠⡈⣿⣿⣿⣿⣇⢹⣿⠘⣠⠔⠉⢈⠑⢦⡁⡇ \n⢻⣿⣯⢰⣎⣡⣥⠄⠄⣀⢀⢀⠸⣷⣿⣿⣿⣿⣿⣇⠟⣴⣿⡀⠄⠄⠄⢸⡇⢸\n⠸⣿⣿⢸⣿⡏⢩⢸⠄⠛⢰⣴⢀⣿⣿⣿⣿⣿⣿⣿⣤⣿⠏⡄⠺⣈⠄⡾⠁⣽\n⣄⢻⣿⢸⣿⣿⡘⣿⣿⣿⣿⠟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢿⠿⣫⣾⠏⣸⢋\n⣿⢠⡹⣇⠹⣿⣷⣜⣛⣛⣣⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⡿⠘⣃⣼ \n⣿⡞⣷⣮⣕⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣴⣾⣯⣿ \n⣿⡇⠻⣿⣿⣿⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣿⣿⣿⣿\n⣿⡇⢷⣎⡹⠿⣿⣿⣿⣿⣿⣿⣿⣏⡉⠙⢛⣋⣵⣾⣿⣿⠿⣃⣶⠻⣿⣏⣿⣿ \n⣿⣇⢸⠿⣛⣥⣶⣭⣝⣛⡻⠿⢿⣿⣿⣿⡿⠿⣫⣭⣶⣶⣿⣿⣿⣷⡛⢻⣿⣿';
+const pardun = 'https://i.imgur.com/4jmjaG3.jpg?1';
+const wataoji = 'https://i.imgur.com/iyCobGd.png?1';
+const yabe = 'https://i.imgur.com/8EzZX5L.gif';
+
 
 function readyDiscord(){
 	console.log('bot ready');
@@ -16,16 +21,28 @@ function readyDiscord(){
 
 function messageHandler(msg) {
 	
+	
 	if(msg.author.bot) return;
 	
 	var emote;
 	
-	if ( msg.content === 'pardun') {
-		emote = 'https://i.imgur.com/4jmjaG3.jpg?1';		
+	if ( msg.content === 'PARDUN') {	
+	const newEmbed = new Discord.MessageEmbed()
+	.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+	.setDescription(ascii_pardun)
+	.setColor('#0c71e0')
+	.setTimestamp();
+	msg.channel.send(newEmbed);
+	console.log('Emote sent by ' + msg.author.tag);
+	msg.delete();
+	return;		
+	
+	} else if ( msg.content === 'pardun') {
+		emote = pardun;		
 	} else if ( msg.content === 'wataoji') {
-		emote = 'https://i.imgur.com/iyCobGd.png?1';		
+		emote = wataoji;		
 	} else if ( msg.content === 'yabe') {
-		emote = 'https://i.imgur.com/8EzZX5L.gif';		
+		emote = yabe;		
 	} else {
 		return;
 	}
@@ -37,8 +54,7 @@ function messageHandler(msg) {
 	.setColor('#0c71e0')
 	.setTimestamp();
 	msg.channel.send(newEmbed);
-	let sender = msg.author.tag;
-	console.log('Emote sent by ${sender}');
+	console.log('Emote sent by ' + msg.author.tag);
 	msg.delete();
 	
 }
