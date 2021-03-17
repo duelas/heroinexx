d9c335c690f9daca2f3203933ceec9c0c52531ad
Hei Ho <heihei1994@hotmail.com>
2021-03-18 05:38:38 +0800

    json test

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const emotes =  require('./emote.json');
+
+
 client.login(process.env.botToken);
 
 client.on('ready', readyDiscord);
@@ -8,11 +11,6 @@ client.on('ready', readyDiscord);
 
 client.on('message' , messageHandler);
 
-
-const ascii_pardun = '⡏⣼⣿⣿⣿⢋⣾⣿⣿⣿⠏⠾⢿⡟⣼⣿⢸⣿⣿⣧⠹⣿⣿⠏⠶⠾⣦⢻⡇⣿ \n⢸⣿⣿⣿⡏⣾⣿⠟⣿⠁⣾⣿⠇⣴⢹⣿⡇⢹⣿⣿⡆⠏⣶⣿⣿⡿⢂⢸⡇⣿ \n⢸⣿⣿⡟⢸⡿⢫⡾⢇⣿⡶⣂⣶⣶⣾⣿⣿⡞⣿⣿⣿⠈⣶⡆⠶⣾⣿⢸⡇⣿ \n⣾⣿⣿⡇⠜⣤⠟⠄⠾⠛⠐⠶⢬⣹⣿⣿⣿⣷⢹⣿⣿⢸⣀⠶⠙⠛⠟⢄⡇⣿ \n⣿⣿⣿⠄⠊⡠⠠⠛⠁⠉⠉⡐⢠⡈⣿⣿⣿⣿⣇⢹⣿⠘⣠⠔⠉⢈⠑⢦⡁⡇ \n⢻⣿⣯⢰⣎⣡⣥⠄⠄⣀⢀⢀⠸⣷⣿⣿⣿⣿⣿⣇⠟⣴⣿⡀⠄⠄⠄⢸⡇⢸\n⠸⣿⣿⢸⣿⡏⢩⢸⠄⠛⢰⣴⢀⣿⣿⣿⣿⣿⣿⣿⣤⣿⠏⡄⠺⣈⠄⡾⠁⣽\n⣄⢻⣿⢸⣿⣿⡘⣿⣿⣿⣿⠟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢿⠿⣫⣾⠏⣸⢋\n⣿⢠⡹⣇⠹⣿⣷⣜⣛⣛⣣⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⡿⠘⣃⣼ \n⣿⡞⣷⣮⣕⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣴⣾⣯⣿ \n⣿⡇⠻⣿⣿⣿⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣿⣿⣿⣿\n⣿⡇⢷⣎⡹⠿⣿⣿⣿⣿⣿⣿⣿⣏⡉⠙⢛⣋⣵⣾⣿⣿⠿⣃⣶⠻⣿⣏⣿⣿ \n⣿⣇⢸⠿⣛⣥⣶⣭⣝⣛⡻⠿⢿⣿⣿⣿⡿⠿⣫⣭⣶⣶⣿⣿⣿⣷⡛⢻⣿⣿';
-const pardun = 'https://i.imgur.com/4jmjaG3.jpg?1';
-const wataoji = 'https://i.imgur.com/iyCobGd.png?1';
-const yabe = 'https://i.imgur.com/8EzZX5L.gif';
 
 
 function readyDiscord(){
@@ -23,39 +21,29 @@ function messageHandler(msg) {
 	
 	if(msg.author.bot) return;
 	
-	var emote;
 	
 	var content = msg.cleanContent;
 	
-	if ( content === 'PARDUN') {	
-	const newEmbed = new Discord.MessageEmbed()
-	.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
-	.setDescription(ascii_pardun)
-	.setColor('#0c71e0')
-	.setTimestamp();
-	msg.channel.send(newEmbed);
-	console.log('Emote sent by ' + msg.author.tag);
-	msg.delete();
-	return;		
+	emotes.forEach(e => {commandChk(e,content);});
 	
-	} else if ( content === 'pardun') {
-		emote = pardun;		
-	} else if ( content === 'wataoji') {
-		emote = wataoji;		
-	} else if ( content === 'yabe') {
-		emote = yabe;		
-	} else {
-		return;
-	}
+}
+
+
+function commandChk(e,content){
 	
+	if (!(content === e.name)) return;	
 	
 	const newEmbed = new Discord.MessageEmbed()
 	.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
-	.setImage(emote)
 	.setColor('#0c71e0')
 	.setTimestamp();
+	
+	if (e.string) { newEmbed.setDescription(e.value); } else { newEmbed.setImage(e.value); }
+	
+	
 	msg.channel.send(newEmbed);
-	console.log('Emote sent by ' + msg.author.tag);
+	console.log(e.name + ' sent by ' + msg.author.tag);
 	msg.delete();
+	
 	
 }
